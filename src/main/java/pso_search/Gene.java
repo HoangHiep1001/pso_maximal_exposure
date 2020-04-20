5e9c00994c3f71573ab5f97cb3696b2e570c23e7
HoangHiep1001 <hiephm1001@gmail.com>
2020-04-20 14:39:22 +0700

    a

--- src/main/java/pso_search/Gene.java
+++ src/main/java/pso_search/Gene.java
@@ -5,5 +5,16 @@ import main.java.model.Point;
 public class Gene extends Point {
     private double exposure;
     private double v;
+    public Gene(double x,double y){
+        super(x,y);
+        this. v = 0;
+    }
 
+    public double getV() {
+        return v;
+    }
+
+    public void setV(double v) {
+        this.v = v;
+    }
 }
--- src/main/java/pso_search/Individual.java
+++ src/main/java/pso_search/Individual.java
@@ -1,4 +1,13 @@
 package main.java.pso_search;
 
+import main.java.model.Objective;
+
 public class Individual {
+    private Objective obj;
+    private Gene[] genes;
+    private double objective;
+    public Individual(){
+
+    }
+
 }
